f24b509cf7eeb0c90c2642f515cc153d342a9267
danindrahmd <91369024+danindrahmd@users.noreply.github.com>
2025-04-02 12:47:36 +1000

    Update Controllable.java

--- src/game/core/Controllable.java
+++ src/game/core/Controllable.java
@@ -3,11 +3,26 @@ package game.core;
 import game.exceptions.BoundaryExceededException;
 import game.utility.Direction;
 
+/**
+ * Represents a controllable object in the game.
+ */
 public abstract class Controllable extends ObjectWithPosition {
+
+    /**
+     * Constructs a controllable object at the given position.
+     *
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public Controllable(int x, int y) {
         super(x, y);
     }
 
+    /**
+     * Moves the controllable object in the specified direction.
+     *
+     * @param direction the direction to move
+     * @throws BoundaryExceededException if movement is outside the game bounds
+     */
     public abstract void move(Direction direction) throws BoundaryExceededException;
-    public abstract void fireBullet();
 }
