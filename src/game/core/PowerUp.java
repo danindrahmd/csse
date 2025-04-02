7cf6837074c5a0342aab80e75bb0cbd25fcf6ca9
danindrahmd <91369024+danindrahmd@users.noreply.github.com>
2025-04-02 12:56:57 +1000

    Update PowerUp.java

--- src/game/core/PowerUp.java
+++ src/game/core/PowerUp.java
@@ -1,17 +1,32 @@
 package game.core;
 
+/**
+ * Abstract class representing a power-up in the game.
+ */
 public abstract class PowerUp extends ObjectWithPosition {
-    protected PowerUpEffect effect;
 
-    public PowerUp(int x, int y, PowerUpEffect effect) {
+    /**
+     * Constructs a power-up at the specified position.
+     *
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
+    public PowerUp(int x, int y) {
         super(x, y);
-        this.effect = effect;
     }
 
-    public void applyEffect(Ship ship) {
-        effect.applyEffect(ship);
-    }
+    /**
+     * Applies the power-up effect to the ship.
+     *
+     * @param ship the ship to apply the effect to
+     */
+    public abstract void applyEffect(Ship ship);
 
+    /**
+     * Moves the power-up down the screen each tick.
+     *
+     * @param tick the current game tick
+     */
     @Override
     public void tick(int tick) {
         y++;
